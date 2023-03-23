8264c019cec11c692550fe354768867b75c209c2
Nikita Tsurikhin <tsurikhinnikita@gmail.com>
2023-03-23 10:53:44 +0600

    first try

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      item =>
+        `<li>
+          <a class="gallery__item" href="${item.original}">
+            <img 
+              class="gallery__image" 
+              src="${item.preview}" 
+              alt="${item.description}" />
+          </a>
+        </li>`
+    )
+    .join('');
+}
+
+galleryUl.innerHTML = createGalleryMarkup(galleryItems);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveStateToLocalStorage = throttle(() => {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+}, 500);
+
+form.addEventListener('input', saveStateToLocalStorage);
+
+const storedState =
+  JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+emailInput.value = storedState.email || '';
+messageInput.value = storedState.message || '';
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log('Form state:', state);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+});
